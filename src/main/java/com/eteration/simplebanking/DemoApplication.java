8af5a9a5f916f2a6b5dba601e927e35b37a46efe
Mehmet Ali Zorba <97551928+malizorba@users.noreply.github.com>
2024-06-01 17:51:04 +0300

    swagger has implemented

--- src/main/java/com/eteration/simplebanking/DemoApplication.java
+++ src/main/java/com/eteration/simplebanking/DemoApplication.java
@@ -2,10 +2,13 @@ package com.eteration.simplebanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 
+@Configuration
 public class DemoApplication {
 
 	public static void main(String[] args) {
